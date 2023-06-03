d92fa5fcdc9bcb40481800cde66fd59e50ddc868
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2023-06-03 18:47:05 +0400

    comment -

--- charting.py
+++ charting.py
@@ -6,7 +6,7 @@ def charting(results) -> list:
     """
     Функция для рисования графика
     """
-    cores, times = zip(*results)  # разделяем данные на два списка
+    cores, times = zip(*results)  
     plt.bar(cores, times, align='center', alpha=0.5)
     plt.title('Время выполнения от количества ядер')
     plt.xlabel('Количество ядер')
